842a54063e76beeee5ce9f0184892ff408498b1f
Ian Smith <iansmith9876@gmail.com>
2018-10-02 21:52:59 -0700

    Add basic functions to property contract

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -4,4 +4,20 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
 contract Property is ERC721Token("Property", "PPT") {
 
+  modifier onlyOwner(uint256 _tokenId) {
+    require(tokenOwner[_tokenId] == msg.sender);
+    _;
+  }
+
+  function createProperty() external {
+    _mint(msg.sender, allTokens.length + 1);
+  }
+
+  function setURI(uint256 _tokenId, string _uri) external onlyOwner(_tokenId) {
+    _setTokenURI(_tokenId, _uri);
+  }
+
+  function getURI(uint256 _tokenId) external view returns(string) {
+    return tokenURIs[_tokenId];
+  }
 }
