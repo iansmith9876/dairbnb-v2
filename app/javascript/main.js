d4f5101e7d2e8f2e288d442f7619310510838457
Ian Smith <iansmith9876@gmail.com>
2018-10-12 23:06:43 -0700

    Run create property transaction based off a button click

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -14,9 +14,30 @@ async function getContract(json, web3 = window.web3) {
   return contract.deployed();
 }
 
+const { accounts } = web3.eth;
+const alice = accounts[0];
+const bob = accounts[1];
+
 async function setContracts() {
   const json = await fetch('../../build/contracts/Property.json').then((res) => res.json());
   propertyContract = await getContract(json);
+
+
+
+  document.querySelector('#create-property').onclick = async () => {
+    try {
+      const tx = await propertyContract.createProperty({
+        from: alice,
+        gas: 250000
+      });
+      console.log(tx);
+      console.log('Property Created for Alice');
+    } catch(e) {
+      console.log(e);
+      alert('Error creating property', e)
+    }
+  }
+
 
   const event = propertyContract.allEvents({ fromBlock: 0, toBlock: 'latest' });
   event.watch((err, res) => {
@@ -28,4 +49,3 @@ async function setContracts() {
 }
 
 setContracts();
-
